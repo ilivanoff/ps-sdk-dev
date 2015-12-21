a7bffc04226bcef37d08426c2bef35303452f218
ilivanoff <ivanov.ilya.alex@gmail.com>
2015-12-22 00:28:47 +0300

    ajax Action

--- ps-sdk/www/ps-ajax.php
+++ ps-sdk/www/ps-ajax.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * Основной скрипт, вызываемый для выполнения всех ajax действий
- */
-require_once __DIR__ . '/ps-includes/ajax/MainImport.php';
-
-execute_ajax_action(AjaxActions::getAction());
+//Просто вызываем скрипт, который и сделает всю работу
+require_once __DIR__ . '/ps-includes/ajax/Action.php';
 ?>
