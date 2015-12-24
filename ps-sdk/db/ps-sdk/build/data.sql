ef6b28dc3dfa00d279bf861d13042c302acfa757
ilivanoff <ivanov.ilya.alex@gmail.com>
2015-12-24 13:50:46 +0300

    ps-addon ignore

--- ps-sdk/db/ps-sdk/build/data.sql
+++ ps-sdk/db/ps-sdk/build/data.sql
@@ -1,6 +1,6 @@
 /*
  * + table users
  */
-insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Администратор системы', 1450953896, 1, 'admin@mail.ru', '96e79218965eb72c92a549dd5a330112', 1, 1, null, null, null, null, null, null, null, null);
-insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Система', 1450953896, 1, 'system@postupayu.ru', '-', 1, 0, null, null, null, null, null, null, null, null);
-insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Илья', 1450953896, 1, 'azaz@mail.ru', '96e79218965eb72c92a549dd5a330112', 0, 1, null, null, null, null, null, null, null, null);
+insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Администратор системы', 1450954057, 1, 'admin@mail.ru', '96e79218965eb72c92a549dd5a330112', 1, 1, null, null, null, null, null, null, null, null);
+insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Система', 1450954057, 1, 'system@postupayu.ru', '-', 1, 0, null, null, null, null, null, null, null, null);
+insert into users (user_name, dt_reg, b_sex, email, passwd, b_admin, b_can_login, id_avatar, about, about_src, contacts, contacts_src, msg, msg_src, timezone) values ('Илья', 1450954057, 1, 'azaz@mail.ru', '96e79218965eb72c92a549dd5a330112', 0, 1, null, null, null, null, null, null, null, null);
