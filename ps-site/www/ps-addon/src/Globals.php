0c59c4851b529995ffcc87e4b672eb5234fa250c
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-01-25 22:42:05 +0300

    Globals.php

---
+++ ps-site/www/ps-addon/src/Globals.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Булева переменная
+ */
+define('PS_GLOBAL_PROPERTY_BOOL', true);
+
+/*
+ * Строковая переменная
+ */
+define('PS_GLOBAL_PROPERTY_STRING', '');
+
+/*
+ * Численная переменная
+ */
+define('PS_GLOBAL_PROPERTY_NUMBER', 1.23);
+
+?>
