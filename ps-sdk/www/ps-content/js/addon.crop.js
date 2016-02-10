3e334dbfa070237923f3e3c1cef51e7807280c0b
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-02-10 21:14:35 +0300

    addon.crop.js

--- ps-sdk/www/ps-content/js/addon.crop.js
+++ ps-sdk/www/ps-content/js/addon.crop.js
@@ -1,5 +1,106 @@
 $(function () {
-    //Сделаем кнопку загрузки файлов
+    
+    var CropUploadLogger = PsLogger.inst('CropUpload').setTrace();
+    /*
+     * Менеджер для работы страницы загрузки картинки
+     */
+    var CropUpload = {
+        //Кнопка загрузки файла
+        $fileSelect: null,
+        //Счётчик выбора файлов
+        fileSelectCounter: 0,
+        //Инициализация менеджера
+        init: function() {
+            this.$fileSelect = $('#choose-file');
+            
+            if (this.$fileSelect.isEmptySet()) {
+                CropUploadLogger.logWarn('Не найдена кнопка загрузки картинки Crop');
+                return;//----
+            }
+            
+            //TODO - проверять, поддерживается ли FileApi
+            this.$fileSelect.change(PsUtil.safeCall(CropUpload.onFileSelected, CropUpload));
+        },
+        
+        //Метод вызывается при выборе файла в поле выбора
+        onFileSelected: function() {
+            //Выбраны ли файлы?
+            if (!this.$fileSelect[0].files.length) {
+                CropUploadLogger.logWarn('Файл не выбран');
+                return;//---
+            }
+            
+            var readId = ++this.fileSelectCounter;
+            var file = this.$fileSelect[0].files[0];
+            
+            CropUploadLogger.logInfo(" >> {}. Выбран файл: '{}' [{}]. Размер: {}.", readId, file.name, file.type, file.size);
+
+            var error = this.validateSelectedFile(file);
+            
+            if (error) {
+                CropUploadLogger.logWarn(" << {}. Файл '{}' не может быть загружен: {}.", readId, file.name, error);
+                alert(error);
+            } else {
+                CropUploadLogger.logTrace('Начинаем чтение файла...');
+                var fileReader = new FileReader();
+                fileReader.addEventListener('load', function (evt) {
+                    this.removeEventListener('load', this, true);
+                    var img = {
+                        id: readId, //Код загрузки
+                        file: file, //Загруженный файл
+                        data: evt.target.result  //Данные из файла
+                    };
+                    CropUpload.onFileReaded(img);
+                }, false);
+                fileReader.readAsDataURL(file);
+            }
+        },
+        
+        //Метод проверяет выбранный файл - его тип и размер
+        validateSelectedFile: function(file) {
+            if(!file.size) {
+                return 'Пустой файл';
+            }
+            if(!file.type.startsWith('image/')) {
+                return 'Тип файлов ['+file.type+'] не поддерживается';
+            }
+            return null;//---
+        },
+        
+        //Метод вызывается, когда файл картинки был успешно прочитан
+        onFileReaded: function(img) {
+            if (img.id == this.fileSelectCounter) {
+                CropUploadLogger.logInfo(" << {}. Файл '{}' успешно прочитан, длина data url: {}", img.id, img.file.name, img.data.length);
+                //CropUploadLogger.logTrace('Содержимое: {}', dataUrl);
+                this.processReadedFile(img);
+            } else {
+                CropUploadLogger.logWarn(" << {}. Файл '{}' успешно прочитан, но в данный момент загружается другой файл.", img.id, img.file.name);
+            }
+        },
+        
+        //Метод вызывается после того, как файл был успешно прочитан - для добавления изображения и перестроения редактора
+        processReadedFile: function(img) {
+            CropUploadLogger.logTrace('Определяем размер картинки.');
+            var ImageObj = new Image();
+            ImageObj.onload = function() {
+                if (img.id == CropUpload.fileSelectCounter) {
+                    img.w = ImageObj.width;
+                    img.h = ImageObj.height;
+                    CropUploadLogger.logInfo("Размер картинки '{}': {}x{}", img.file.name, img.w, img.h);
+                }
+            };
+            ImageObj.src = img.data;
+        }
+    }
+    
+    CropUpload.init();
+    
+    return;//---
+    /*
+     * Сделаем кнопку загрузки файлов
+     * TODO - использовать в случае не поддержки FileApi
+     */
+    /*
     $('#file_upload').psUploadify({
         formData: {
             type: 'Crop'
@@ -9,7 +110,9 @@ $(function () {
         },
         buttonText: 'Загрузить картинку'
     });
+    */
 
+    
     var $cropper = $('#image').cropper({
         aspectRatio: 1,
         preview: '.crop-preview, .crop-preview-small',
@@ -20,24 +123,25 @@ $(function () {
         zoomable: false,
         viewMode: 1,
         crop: function (data) {
-
-            $('.container').append($('<img>').attr('src', $cropper.cropper('getCroppedCanvas').toDataURL()));
-
+        
+        //$('.container').append($('<img>').attr('src', $cropper.cropper('getCroppedCanvas').toDataURL()));
+        
         },
         built: function () {
             //$cropper.cropper('disable');
-
+            
+            return;//---
             PsUtil.scheduleDeferred(function () {
-
+                
                 $('.container').append($('<img>').attr('src', $cropper.cropper('getCroppedCanvas').toDataURL()));
-
-                AjaxExecutor.execute('CropUpload', {
+                
+                AjaxExecutor.executePost('CropUpload', {
                     data: $cropper.cropper('getCroppedCanvas').toDataURL()
                 },
-                        function (ok) {
-                            alert('OK: ' + ok);
-                        })
-
+                function (ok) {
+                    alert('OK: ' + ok);
+                })
+            
             }, null, 1000);
         }
     });
--- ps-sdk/www/ps-includes/smarty/templates/crop/page.tpl
+++ ps-sdk/www/ps-includes/smarty/templates/crop/page.tpl
@@ -1,7 +1,9 @@
 <h1 class="head">Публикатор мыслей</h1>
 
 <div class="container">
-    <input id="file_upload" name="file_upload" type="file" />
+    {*<input id="file_upload" name="file_upload" type="file" />*}
+
+    <input type="file" id="choose-file" />
 
     <!-- Wrap the image or canvas element with a block element -->
     <div class="crop-holder">
@@ -9,7 +11,7 @@
         {*<img id="image" src="http://cdn3.vox-cdn.com/uploads/chorus_asset/file/917470/iphone-6-travel-photo-review-mann-header.0.jpg">*}
         {*<img id="image" src="http://i.ebayimg.com/images/i/161875836128-0-1/s-l1000.jpg">*}
         {*<img id="image" src="http://newshour-tc.pbs.org/newshour/wp-content/uploads/2015/04/Jupiter.jpg">*}
-        <img id="image" src="http://cs402630.vk.me/v402630276/839b/fDvUp-RylzM.jpg">
+        {*<img id="image" src="http://cs402630.vk.me/v402630276/839b/fDvUp-RylzM.jpg">*}
         {*<img id="image" src="http://www.glcambodia.com/wp-content/uploads/2014/06/%D0%B0%D0%BD%D0%B3%D0%BA%D0%BE%D1%80-%D0%B2%D0%B0%D1%82-%D0%B2%D0%B5%D1%87%D0%B5%D1%80%D0%BE%D0%BC.jpg">*}
     </div>
 
